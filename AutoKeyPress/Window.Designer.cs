3233a6bce03f038b82120c5953d18314874d8d5b
floathandthing <springtrap.bite87@gmail.com>
2018-11-14 19:19:43 +0000

    Proof of concept UI. It technically works, but its not really useful yet.

--- AutoKeyPress/Window.Designer.cs
+++ AutoKeyPress/Window.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.command_input = new System.Windows.Forms.TextBox();
+            this.execute_btn = new System.Windows.Forms.Button();
+            this.timer_display = new System.Windows.Forms.Label();
+            this.ABORTAREA = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // command_input
+            // 
+            this.command_input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.command_input.Location = new System.Drawing.Point(18, 18);
+            this.command_input.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.command_input.Multiline = true;
+            this.command_input.Name = "command_input";
+            this.command_input.Size = new System.Drawing.Size(388, 164);
+            this.command_input.TabIndex = 0;
+            // 
+            // execute_btn
+            // 
+            this.execute_btn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.execute_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.execute_btn.Location = new System.Drawing.Point(18, 194);
+            this.execute_btn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.execute_btn.Name = "execute_btn";
+            this.execute_btn.Size = new System.Drawing.Size(390, 52);
+            this.execute_btn.TabIndex = 1;
+            this.execute_btn.Text = "Start";
+            this.execute_btn.UseVisualStyleBackColor = true;
+            this.execute_btn.Click += new System.EventHandler(this.execute_btn_Click);
+            // 
+            // timer_display
+            // 
+            this.timer_display.AutoSize = true;
+            this.timer_display.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer_display.Location = new System.Drawing.Point(17, 251);
+            this.timer_display.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timer_display.Name = "timer_display";
+            this.timer_display.Size = new System.Drawing.Size(43, 25);
+            this.timer_display.TabIndex = 2;
+            this.timer_display.Text = "Idle";
+            // 
+            // ABORTAREA
+            // 
+            this.ABORTAREA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ABORTAREA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ABORTAREA.Location = new System.Drawing.Point(22, 320);
+            this.ABORTAREA.Multiline = true;
+            this.ABORTAREA.Name = "ABORTAREA";
+            this.ABORTAREA.ReadOnly = true;
+            this.ABORTAREA.Size = new System.Drawing.Size(384, 70);
+            this.ABORTAREA.TabIndex = 3;
+            this.ABORTAREA.Text = "\r\nPut mouse in here to abort!";
+            this.ABORTAREA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ABORTAREA.MouseEnter += new System.EventHandler(this.ABORTAREA_MouseEnter);
             // 
             // Window
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(426, 402);
+            this.Controls.Add(this.ABORTAREA);
+            this.Controls.Add(this.timer_display);
+            this.Controls.Add(this.execute_btn);
+            this.Controls.Add(this.command_input);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Window";
             this.Text = "Auto Key Presser";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox command_input;
+        private System.Windows.Forms.Button execute_btn;
+        private System.Windows.Forms.Label timer_display;
+        private System.Windows.Forms.TextBox ABORTAREA;
     }
 }
 
--- AutoKeyPress/Window.cs
+++ AutoKeyPress/Window.cs
@@ -5,16 +5,65 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace AutoKeyPress
 {
     public partial class Window : Form
     {
+        bool is_running = false;
+        Thread DoKeystrokes;
+
         public Window()
         {
             InitializeComponent();
+             DoKeystrokes = new Thread(execute_keystrokes); 
+        }
+
+        private void execute_btn_Click(object sender, EventArgs e)
+        {
+            if (is_running)
+            {
+                DoKeystrokes.Abort();
+                DoKeystrokes = new Thread(execute_keystrokes);
+                execute_btn.Text = "Start";
+            }
+            else
+            {
+                DoKeystrokes.Start();
+                execute_btn.Text = "Stop";
+            }
+        }
+
+        void execute_keystrokes()
+        {
+            is_running = true;
+            for (int i = 5; i > 0; i--)
+            {
+                Invoke(new Action(() => { timer_display.Text = i+" seconds remaining"; }));
+                Thread.Sleep(1000);
+            }
+            Invoke(new Action(() =>
+            {
+                timer_display.Text = "0 seconds remaining";
+            }));
+
+            string data = command_input.Text.Replace(Environment.NewLine, "");
+            Invoke(new Action(() => { SendKeys.Send(data); })); 
+            
+            is_running = false;
+        }
+
+        private void ABORTAREA_MouseEnter(object sender, EventArgs e)
+        {
+            if (is_running)
+            {
+                DoKeystrokes.Abort();
+                DoKeystrokes = new Thread(execute_keystrokes);
+                timer_display.Text = "ABORTED";
+                execute_btn.Text = "Start";
+            }
         }
     }
 }
